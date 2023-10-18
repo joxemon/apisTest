7ab4ab09fe554cc38431e0b0ae9a83a8085f7abe
josemaria <josemaria.gonzalez@entelgydigital.com>
2023-10-18 11:33:07 +0200

    first commit

---
+++ apis/src/test/java/com/joxemon/apis/ApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joxemon.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
